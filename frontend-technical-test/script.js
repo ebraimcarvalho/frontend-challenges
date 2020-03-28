0147328e3a688d41dddfc09da3e237c260cec79f
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-03-28 15:38:24 -0300

    style event__option

--- frontend-technical-test/script.js
+++ frontend-technical-test/script.js
@@ -22,33 +22,38 @@
 
   function renderEvents(data) {
     var docFragm = doc.createDocumentFragment();
-    var divRender = doc.createElement('div');
     console.log('loaded', data);
     for (let i = 0, dataLength = data.length; i < dataLength; i++) { //Percorre todo array
       if (data[i].markets.length > 0) { //Entra apenas no array que tem markets
+        var divRender = doc.createElement('div');
+        divRender.setAttribute('class', 'event__option');
         divRender.innerHTML += `
           <div class="event__title">
             <span class="event__title-name">${data[i].name}</span>
           </div>
         `;
         for (let j = 0, marketsLength = data[i].markets.length; j < marketsLength; j++) { //Percorre os markets
-          divRender.innerHTML += `
-          <div class="event__market-${j}">
+          var divEventMarket = doc.createElement('div');
+          divEventMarket.setAttribute('class', 'event__market');
+          divEventMarket.innerHTML += `
+          <div class="event__market-${j+1}">
             <h3 class="event__market-type">${data[i].markets[j].name}</h3>`;
-          for (let k = 0, selectionsLength = data[i].markets[j].selections.length; k < selectionsLength; k++) {
-            divRender.innerHTML += `
+            for (let k = 0, selectionsLength = data[i].markets[j].selections.length; k < selectionsLength; k++) {
+              divEventMarket.innerHTML += `
                 <button class="market__select-${j}">
                   ${data[i].markets[j].selections[k].name} <br/>
                   ${data[i].markets[j].selections[k].price}
                 </button>
-            </div>
+              </div>
           `;
           }
-        }
-      }
+          divRender.appendChild(divEventMarket);         
+        }        
+        docFragm.appendChild(divRender);
+        eventDiv.appendChild(docFragm);
+      }      
     }
-    docFragm.appendChild(divRender);
-    eventDiv.appendChild(docFragm);
+    
   }
 
   getApi();
