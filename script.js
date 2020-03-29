c73213403b1357211cef7c43daa60fd1cec472e4
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-03-29 04:32:38 -0300

    frontend-technical-test beta

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 
   const projects = [
     {
+      name: 'frontend-technical-test',
+      linkChallenge: 'https://github.com/ebraimcarvalho/frontend-challenges/blob/master/frontend-technical-test/README.md'
+    },
+    {
       name: 'base-apparel-coming-soon-master',
       linkChallenge: 'https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0'
     },
