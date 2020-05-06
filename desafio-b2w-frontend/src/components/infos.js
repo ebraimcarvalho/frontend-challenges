e480947db50596fd657837d6c1b7fb20cc9d611c
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-05-06 15:02:12 -0300

    finish style

--- desafio-b2w-frontend/src/components/infos.js
+++ desafio-b2w-frontend/src/components/infos.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Infos = ({ title, info }) => (
-  <div>
-    <h3>{title}</h3>
+  <div className="info">
+    <h3 className="info__title">{title.toUpperCase()}</h3>
     {info.map((item, index) => (
-      <div className="info" key={index}>
-        <p className="info__name">{item.name}</p>
+      <div className="info__list" key={index}>
+        <p className="info__name">{item.name.toUpperCase()}</p>
         <p className="info__date">{item.date}</p>
         <p className="info__description">{item.description}</p>
       </div>
