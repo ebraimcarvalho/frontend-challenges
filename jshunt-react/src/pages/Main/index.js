751d873103ec51aeb78aa3e4acd2ed89f0285ff3
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-05-15 03:46:17 -0300

    config routes in Main

--- jshunt-react/src/pages/Main/index.js
+++ jshunt-react/src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import api from '../../services/api'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 
@@ -47,7 +48,7 @@ class Main extends React.Component {
           <article key={product._id}>
             <h3>{product.title}</h3>
             <p>{product.description}</p>
-            <a href="">Acessar</a>
+            <Link to={`/products/${product._id}`}>Acessar</Link>
           </article>
         ))}
         <div className='actions'>
