53d91efe65703cf9330dfb64005f6b908411a60a
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-06-14 02:19:01 -0300

    redux in reservation page

--- travel-reservation/src/pages/Home/index.js
+++ travel-reservation/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { MdFlightTakeoff } from 'react-icons/md';
 import api from '../../server.json'
 
--- travel-reservation/src/pages/Reservation/index.js
+++ travel-reservation/src/pages/Reservation/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 import { MdDelete } from 'react-icons/md'
 import './styles.css';
 
 function Reservation() {
+  const reserves = useSelector(state => state.reserve)
+  console.log(reserves)
+
   return(
     <div className="reservation">
-      <h1 className="reservation__title">Você tem 2 reservas</h1>
+      <h1 className="reservation__title">Você tem {reserves.length} {reserves.length <= 1 ? 'reserva' : 'reservas'}</h1>
       <div className="reservation__box">
-        <div className="reservation__item">
-          <img className="reservation__image" src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="Maceió"/>
-          <div className="reservation__div-text">
-            <h3 className="reservation__text">Viagem Maceió 7 Dias</h3>
-            <span className="reservation__span">Quantidade: 2</span>
+
+        {reserves.map(reserve => (
+          <div key={reserve.id} className="reservation__item">
+            <img className="reservation__image" src={reserve.image} alt={reserve.title} />
+            <div className="reservation__div-text">
+              <h3 className="reservation__text">{reserve.title}</h3>
+              <span className="reservation__span">Quantidade: 2</span>
+            </div>
+            <button className="reservation__button" type="button" onClick={() => {}}>
+              <MdDelete size={20} color="#191919" />
+            </button>
           </div>
-          <button className="reservation__button" type="button" onClick={() => {}}>
-            <MdDelete size={20} color="#191919" />
-          </button>
-        </div>
+        ))}
+        
         
       </div>
 
